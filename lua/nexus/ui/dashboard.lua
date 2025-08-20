00a19d561e6e7e5ae9d9d6679239bbf91aa78080
Jeremiah Smith <jeremiah@jeremiahsmith.dev>
2025-08-20 17:56:31 +0700

    fix: center dashboard icons properly

--- lua/nexus/ui/dashboard.lua
+++ lua/nexus/ui/dashboard.lua
@@ -7,12 +7,12 @@ function M.get_dashboard_buttons(config)
   
   return {
     "",
-    "    󰈞  Find file                   SPC f",
-    "    󰋚  Recently opened files       SPC r", 
-    "    󰊄  Find word                   SPC w",
-    "    󰈔  New file                    SPC n",
-    "    󰃃  Bookmarks                   SPC b",
-    "    󰁯  Restore session             SPC s",
+    "󰈞  Find file                   SPC f",
+    "󰋚  Recently opened files       SPC r", 
+    "󰊄  Find word                   SPC w",
+    "󰈔  New file                    SPC n",
+    "󰃃  Bookmarks                   SPC b",
+    "󰁯  Restore session             SPC s",
     ""
   }
 end
